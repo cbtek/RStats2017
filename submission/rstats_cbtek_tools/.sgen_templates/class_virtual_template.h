9e353c2086b60cab94f7261814072424c7d69293
CBTek - zeroefusion <corey.berry@cbtek.net>
2017-04-20 21:39:17 -0400

    Updated documentation Found new place for sgen/pgen

---
+++ submission/rstats_cbtek_tools/.sgen_templates/class_virtual_template.h
@@ -0,0 +1,18 @@
+/*
+    $CLASS_NAME.hpp
+    $COPYRIGHT
+*/
+#pragma once
+$INCLUDES_H
+$BEGIN_NAMESPACE
+
+class $CLASS_NAME $PARENT_CLASSES
+{
+public:
+    /**	
+    * @brief Virtual descructor for $CLASS_NAME
+    *
+	*/	
+    virtual ~$CLASS_NAME(){}
+};
+$END_NAMESPACE
