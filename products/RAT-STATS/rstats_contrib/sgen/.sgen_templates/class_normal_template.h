4726bffc6de300baaadbdb3dd94dd4c79f75b3c1
CBTek - zeroefusion <corey.berry@cbtek.net>
2017-04-20 21:00:24 -0400

    Added pgen / sgen to contrib folder in RAT-STATS Organized submission folder

---
+++ products/RAT-STATS/rstats_contrib/sgen/.sgen_templates/class_normal_template.h
@@ -0,0 +1,27 @@
+/*
+    $CLASS_NAME.h
+    $COPYRIGHT
+*/
+#pragma once
+$INCLUDES_H
+$BEGIN_NAMESPACE
+class $CLASS_NAME $PARENT_CLASSES
+{
+public:
+   /*
+    * @brief $CLASS_NAME
+    *
+	*/
+	$CLASS_NAME();
+    $CLASS_SETTERS_H
+    $CLASS_GETTERS_H
+	/*
+	 * @brief $CLASS_NAME (Descructor)
+     *
+	 */
+	~$CLASS_NAME();	
+
+private:
+    $CLASS_MEMBERS_H
+};
+$END_NAMESPACE
---
+++ submission/rstats_source/products/RAT-STATS/rstats_contrib/sgen/.sgen_templates/class_singleton_template.h
@@ -0,0 +1,34 @@
+/*
+    $CLASS_NAME.h
+    $COPYRIGHT
+*/
+#pragma once
+$INCLUDES_H
+$BEGIN_NAMESPACE
+class $CLASS_NAME $PARENT_CLASSES
+{
+public:
+	/**
+    * @brief $CLASS_NAME
+    *
+	*/
+	$CLASS_NAME();
+    $CLASS_SETTERS_H
+    $CLASS_GETTERS_H		
+	/**	
+    * @brief Static instance method for this singleton
+    *
+	*/	
+    static $CLASS_NAME & inst();
+
+	/**
+    * @brief $CLASS_NAME (Descructor)
+    *
+	*/
+	~$CLASS_NAME();	
+
+private:
+    static $CLASS_NAME m_instance;
+    $CLASS_MEMBERS_H
+};
+$END_NAMESPACE
